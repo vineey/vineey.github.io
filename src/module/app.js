7412960dc6942b0522d33fdcacbf78f7541bf406
vineey <vinetech416@gmail.com>
2015-10-24 17:45:17 +0800

    remove token authentication on firebase

--- src/module/app.js
+++ src/module/app.js
@@ -17,18 +17,9 @@
 
           $rootScope.head.title = 'Engineering Simplicity';
 
-          var ref = new Firebase("https://vineey.firebaseio.com/users/user1");
-          var AUTH_TOKEN = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2IjowLCJkIjp7InVpZCI6InVzZXIiLCJkYXRhIjp7InByb3ZpZGVyIjoiR2l0aHViIn19LCJpYXQiOjE0NDUwNzcyMjZ9.9IepIGqcClcVUuvoKkVXp3IuZaigpUXc3JkpeiTfuH8';
+          var ref = new Firebase("https://vineey.firebaseio.com/blogs");
 
-          ref.authWithCustomToken(AUTH_TOKEN, function(error, authData) {
-              if (error) {
-                  console.log("Login Failed!", error);
-              } else {
-                  console.log("Login Succeeded!", authData);
-              }
-          });
-
-          $rootScope.user = $firebaseObject(ref);
+          $rootScope.blogs = $firebaseObject(ref);
 
       }]);
 
